a67d2bc3686869e8cb5e2fd958d227e22146c187
Tomas Figueroa <teft90@gmail.com>
2016-04-07 09:18:35 -0600

    Views: Gradient view set to update gradient bounds after view's constraints are modified

--- Phun App/Views/APSGradientImageView.m
+++ Phun App/Views/APSGradientImageView.m
@@ -8,8 +8,67 @@
 
 #import "APSGradientImageView.h"
 
+@interface APSGradientImageView ()
+
+@property (strong, nonatomic) CAGradientLayer *gradientLayer;
+
+@end
+
 @implementation APSGradientImageView
 
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super initWithCoder:aDecoder];
+    if (self) {
+        [self addGradient];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self addGradient];
+    }
+    return self;
+}
+
+- (instancetype)initWithImage:(UIImage *)image
+{
+    self = [super initWithImage:image];
+    if (self) {
+        [self addGradient];
+    }
+    return self;
+}
+
+- (instancetype)initWithImage:(UIImage *)image highlightedImage:(UIImage *)highlightedImage
+{
+    self = [super initWithImage:image highlightedImage:highlightedImage];
+    if (self) {
+        [self addGradient];
+    }
+    return self;
+}
+
+- (void)addGradient
+{
+    // Agregar gradiente a partir del 40% de la imagen hasta el final
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    self.gradientLayer = gradientLayer;
+    gradientLayer.frame = self.bounds;
+    gradientLayer.startPoint = CGPointMake(0, 0);
+    gradientLayer.endPoint = CGPointMake(0, 1);
+    gradientLayer.colors = @[ (id)[UIColor clearColor].CGColor, (id)[UIColor whiteColor].CGColor ];
+    [self.layer addSublayer:gradientLayer];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+    self.gradientLayer.frame = self.bounds;
+}
 /*
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
